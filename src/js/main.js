52c860299688dfc0c480dde823fbbd55a5975147
irati88 <irati.a88@gmail.com>
2025-09-02 21:34:50 +0200

    Fetch data from the API, start the app, store data in an array and create the function to paint the elements in the HTML

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+// 3- Guardamos la info que tenemos en "data" en el array "products"
+
+let products = []; // Cambia de "const" a "let" para poderla reutilizar
+
+// 1- Cogemos los datos de la API con "fetch"
+
+// Creamos una variable para guardar una función que recoja los datos del API
+const getData = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      products = data; // Guarda los 20 elementos del array
+      showProducts();
+    });
+};
+
+// 4- En una variable, guardamos la función que recorra el array y pinte los elementos en el HTML
+
+const showProducts = () => {
+    
+};
+
+// 2- Arrancamos la aplicación
+getData(); // Invocamos la función
+
+
+
+// 5- Escuchar eventos en los productos
